60d55e50f2883a952c86a007fa4991d396f74b8f
Rathod <Prachi.Rathod@philips.com>
2026-04-03 16:42:10 +0530

    Deploy Spring Boot app to ECS Fargate

--- src/main/java/com/example/demo/LambdaHandler.java
+++ src/main/java/com/example/demo/LambdaHandler.java
@@ -1,4 +1,6 @@
-package com.example.demo;
+package com.example;
 
-public class LambdaHandler {
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class LambdaHandler extends SpringBootRequestHandler<Object, Object> {
 }
