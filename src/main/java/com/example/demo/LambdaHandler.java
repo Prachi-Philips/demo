f80634a41371dae3dcd5b23499f291b83888d989
Rathod <Prachi.Rathod@philips.com>
2026-04-03 16:59:06 +0530

    Remove LambdaHandler for ECS deployment

--- src/main/java/com/example/demo/LambdaHandler.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class LambdaHandler extends SpringBootRequestHandler<Object, Object> {
-}
