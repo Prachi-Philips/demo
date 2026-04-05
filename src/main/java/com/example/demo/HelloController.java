89fcfc33da79ecf60382d810c43cd011e6ba9050
Rathod <Prachi.Rathod@philips.com>
2026-04-05 15:32:57 +0530

    verify private subnet deployment

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello GitHub Actions!";
+        return "Hello GitHub Actions from prvate sunbets attached also!";
     }
 }
